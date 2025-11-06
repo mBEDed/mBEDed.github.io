b0aa674cda7bc9158a10a7e50579caa0e6aa5714
userHarpreet <69069980+userHarpreet@users.noreply.github.com>
2025-11-06 14:31:07 +0530

    Services Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 
         // testimonial sliders
         $(".testimonial-sliders").owlCarousel({
-            items: 1,
+            items: 3,
             loop: true,
             autoplay: true,
             responsive: {
